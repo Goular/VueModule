71ac6b0e3a47536ca08f48a5da3a6c08b34881cf
Goular <zhaojt_cs@126.com>
2017-12-02 22:11:38 +0800

    提交内容

--- 02饿了吗Vue/02-ele外卖实战/src/components/header/header.vue
+++ 02饿了吗Vue/02-ele外卖实战/src/components/header/header.vue
@@ -1,11 +1,9 @@
 <template>
-
+  <div>Hello Goular，Welcome to Header.</div>
 </template>
 
 <script text="text/ecmascript-6">
-
 </script>
 
-<style lang="stylus">
-
+<style lang="stylus" rel="stylesheet/stylus">
 </style>
--- 02饿了吗Vue/02-ele外卖实战/src/router/index.js
+++ 02饿了吗Vue/02-ele外卖实战/src/router/index.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import App from '@/App.vue'
+import goods from "@/components/goods"
+import ratings from "@/components/ratings/ratings.vue"
+import seller from "@/components/seller/ratings.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: "/",
+      name: "index",
+      component: App,
+      children: [
+        {
+          path:"/goods",
+          component:goods
+        },
+        {
+          path:"/ratings",
+          component:ratings
+        },
+        {
+          path:"/seller",
+          component:seller
+        }
+      ]
+    }
   ]
 })
