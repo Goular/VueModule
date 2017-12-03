aee7db4e3029a83000189ac26a8cfc9ae83d1236
Goular <zhaojt_cs@126.com>
2017-12-03 17:33:00 +0800

    提交内容

--- 02饿了吗Vue/02-ele外卖实战/src/main.js
+++ 02饿了吗Vue/02-ele外卖实战/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
 
@@ -8,6 +9,9 @@ import router from './router'
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+
+// 添加网络请求库Vue-Resource
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
