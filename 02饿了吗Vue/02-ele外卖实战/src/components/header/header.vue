376f5820f457b6a25034d0d4c9d45aceebe2815e
Goular <zhaojt_cs@126.com>
2017-12-04 15:00:28 +0800

    提交内容

--- 02饿了吗Vue/02-ele外卖实战/src/components/header/header.vue
+++ 02饿了吗Vue/02-ele外卖实战/src/components/header/header.vue
@@ -17,7 +17,7 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
@@ -29,6 +29,12 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%"/>
     </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main"></div>
+      </div>
+      <div class="detail-close"></div>
+    </div>
   </div>
 </template>
 
@@ -39,8 +45,18 @@
         type: Object
       }
     },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice']
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
+      }
     }
   }
 </script>
@@ -161,4 +177,25 @@
       height: 100%
       z-index: -1
       filter: blur(10px)
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        min-height: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
