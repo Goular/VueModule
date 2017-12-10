79587f06251c3d34ebae38ac19f13cc2896562a5
Goular <zhaojt_cs@126.com>
2017-12-10 10:22:45 +0800

    提交购物车内容

---
+++ 02饿了吗Vue/02-ele外卖实战/src/components/shopcart/shopcart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script text="text/ecmascript-6">
+  export default {
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "shopcart.styl"
+</style>
