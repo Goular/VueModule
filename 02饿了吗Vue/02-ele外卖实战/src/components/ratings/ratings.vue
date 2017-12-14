56848bc5baf71dee02df2a4561c11e2be7673e9a
Goular <zhaojt_cs@126.com>
2017-12-14 16:43:45 +0800

    提交内容

--- 02饿了吗Vue/02-ele外卖实战/src/components/ratings/ratings.vue
+++ 02饿了吗Vue/02-ele外卖实战/src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" ref="ratings">
     <div class="ratings-content">
       <div class="overview">
         <div class="overview-left">
@@ -27,14 +27,42 @@
       <split></split>
       <ratingselect :ratings="ratings" :select-type="selectType"
                     :only-content="onlyContent"></ratingselect>
+      <div class="rating-wrapper border-1px">
+        <ul>
+          <li v-for="rating in ratings" class="rating-item" v-show="needShow(rating.rateType, rating.text)">
+            <div class="avatar">
+              <img :src="rating.avatar" alt="" width="28" height="28">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">
+                {{rating.deliveryTime}}
+              </span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend &&rating.recommend.length">
+                <i class="iconfont icon-damuzhi"></i>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script text="text/ecmascript-6">
+  import BScroll from 'better-scroll'
   import star from 'components/star/star'
   import ratingselect from 'components/ratingselect/ratingselect'
   import split from 'components/split/split'
+  import {formatDate} from 'common/js/date'
 
   // eslint-disable-next-line
   const POSITIVE = 0
@@ -64,10 +92,9 @@
       this.$http.get('http://www.blog.com/api/ratings').then(response => {
         let obj = response.body
         if (obj.status === ERR_OK) {
-          this.ratings = obj.data
           this.$nextTick(() => {
-            // this._initScroll()
-            // this._calculateHeight()
+            this.ratings = obj.data
+            this.scroll = new BScroll(this.$refs.ratings, {click: true})
           })
         }
       }, response => {
@@ -82,45 +109,64 @@
       star,
       ratingselect,
       split
+    },
+    methods: {
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false
+        }
+        if (this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType
+        }
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd hh:mm')
+      }
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet">
+  @import "../../common/stylus/mixin.styl"
+
   .ratings
-    position: absolute
-    top: 174px
-    bottom: 0
-    left: 0
-    width: 100%
-    overflow: hidden
+    position absolute
+    top 174px
+    bottom 0
+    left 0
+    width 100%
+    overflow hidden
     .overview
-      display: flex
-      padding: 18px 0
+      display flex
+      padding 18px 0 18px 18px
       .overview-left
-        flex: 0 0 137px
-        padding: 6px 0
-        width: 137px
-        border-right: 1px solid rgba(7, 17, 27, 0.1)
-        text-align: center
-        /* 解决在IPhone5下显示异常的问题，需要采用媒体搜索*/
-        @media only screen and (max-width: 320px)
-          flex: 0 0 110px
-          width: 110px
+        flex 0 0 137px
+        width 137px
+        padding 6px 0px
+        border-right 1px solid rgba(7, 17, 27, 0.1)
+        text-align center
+        @media only screen and (max-width 320px)
+          flex 0 0 110px
+          width 110px
         .score
-          margin-bottom: 6px
-          line-height: 28px
-          font-size: 24px
-          color: rgb(255, 153, 0)
+          margin-bottom 12px
+          line-height 28px
+          font-size 24px
+          color rgb(255, 153, 0)
         .title
-          margin-bottom: 8px
-          line-height: 12px
-          font-size: 12px
-          color: rgb(7, 17, 27)
+          margin-bottom 8px
+          line-height 12px
+          font-size 12px
+          color rgb(7, 17, 27)
         .rank
-          line-height: 10px
-          font-size: 10px
-          color: rgb(147, 153, 159)
+          line-height 10px
+          font-size 12px
+          color rgb(147, 153, 159)
       .overview-right
         flex 1
         padding 6px 0 6px 24px
@@ -160,5 +206,67 @@
             vertical-align top
             line-height 18px
             font-size 12px
+            color rgb(147, 153, 159)
+
+    .rating-wrapper
+      padding 0 18px
+      .rating-item
+        display flex
+        padding 18px 0
+        border-1px(rgba(1, 17, 27, 0.1))
+        .avatar
+          flex 0 0 28px
+          width 28px
+          margin-right 12px
+          img
+            border-radius 50%
+        .content
+          position relative
+          flex 1
+          .name
+            margin-bottom 4px
+            line-height 12px
+            font-weight 700
+            font-size 10px
+            color rgb(7, 17, 27)
+          .star-wrapper
+            margin-bottom 6px
+            font-size 0
+            .star
+              display inline-block
+              margin-right 16px
+              vertical-align top
+            .delivery
+              display inline-block
+              vertical-align top
+              font-size 10px
+              line-height 12px
+              color rgb(147, 153, 159)
+          .text
+            line-height 18px
+            color rgb(7, 17, 27)
+            font-size 12px
+            margin-bottom 8px
+          .recommend
+            line-height 16px
+            font-size 0
+            .iconfont, .item
+              display inline-block
+              margin 0 8px 4px 0
+              font-size 9px
+            .iconfont
+              color rgb(0, 160, 220)
+            .item
+              padding 0 6px
+              border 1px solid rgba(7, 17, 27, 0.1)
+              border-radius 1px
+              color rgb(147, 153, 159)
+              background #fffff
+          .time
+            position absolute
+            top 0
+            right 0
+            line-height 12px
+            font-size 10px
             color rgb(147, 153, 159)
 </style>
