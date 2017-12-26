9f5b0fd685b57e5bae640262391b7cd193c9212d
Goular <zhaojt_cs@126.com>
2017-12-26 17:17:07 +0800

    提交内容

---
+++ 02项目实战/02-ele外卖实战/jenkins.sh
@@ -0,0 +1,6 @@
+# 用于jenkin项目中，构建配置时"构建"环节的Execute shell,添加下面内容，$WORKSPACE为jenkins自带的环境变量
+
+cd $WORKSPACE'/02项目实战/02-ele外卖实战'
+docker run -v $PWD:/app drnmp_node npm install
+docker run -v $PWD:/app drnmp_node npm run build
+tar -zcvf eleme.tar.gz ./dist/
