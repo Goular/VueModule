7963ba1b94f579ff6d17f6181404c69817971602
Goular <zhaojt_cs@126.com>
2018-01-13 15:30:47 +0800

    提交内容

--- 02项目实战/03.Vue-Music/src/api/recommend.js
+++ 02项目实战/03.Vue-Music/src/api/recommend.js
@@ -1,6 +1,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
+// 获取推荐内容
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
 
@@ -11,5 +12,21 @@ export function getRecommend() {
     needNewCode: 1
   })
 
+  return jsonp(url, data, options)
+}
+
+// 获取歌单内容
+export function getDiscList() {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random()
+  })
   return jsonp(url, data, options)
 }
