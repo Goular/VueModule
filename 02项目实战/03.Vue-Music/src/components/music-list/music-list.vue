33b41cc6b5ee9d18a5bcc32ef6cc50bc99085232
Goular <zhaojt_cs@126.com>
2018-01-17 14:49:00 +0800

    提交内容

--- 02项目实战/03.Vue-Music/src/components/music-list/music-list.vue
+++ 02项目实战/03.Vue-Music/src/components/music-list/music-list.vue
@@ -5,7 +5,7 @@
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list"
@@ -68,8 +68,21 @@
       scrollY(newY) {
         let tranlateY = Math.max(this.minTranslateY, newY)
         let zIndex = 0
+        let scale = 1
+        let blur = 0
         this.$refs.layer.style[`transform`] = `translate3d(0,${tranlateY}px,0)`
         this.$refs.layer.style[`webkitTransform`] = `translate3d(0,${tranlateY}px,0)`
+        const percent = Math.abs(newY / this.imageHeight)
+        if (newY > 0) {
+          //  即往下拉的时候，进行图片的放大
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(20 * percent, 20)
+        }
+        // 歌手页面进行高斯模糊
+        this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
+        this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
         if (newY < this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
@@ -79,6 +92,8 @@
           this.$refs.bgImage.style.height = 0
         }
         this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style[`transform`] = `scale(${scale})`
+        this.$refs.bgImage.style[`webkitTransform`] = `scale(${scale})`
       }
     },
     components: {
