9780f16fb932bd0b7d439b7ba415701e81f283f0
Goular <zhaojt_cs@126.com>
2018-01-18 11:35:27 +0800

    提交内容

--- 02项目实战/03.Vue-Music/src/store/getters.js
+++ 02项目实战/03.Vue-Music/src/store/getters.js
@@ -1,1 +1,18 @@
 export const singer = state => state.singer
+
+export const playing = state => state.playing
+
+export const fullScreen = state => state.fullScreen
+
+export const playList = state => state.playList
+
+export const sequenceList = state => state.sequenceList
+
+export const mode = state => state.mode
+
+export const currentIndex = state => state.currentIndex
+
+// 由于不是简写，所以不能这样像上面那样是简写
+export const currentSong = (state) => {
+  return state.playList[state.currentIndex] || {}
+}
--- 02项目实战/03.Vue-Music/src/store/state.js
+++ 02项目实战/03.Vue-Music/src/store/state.js
@@ -1,4 +1,18 @@
+import {playMode} from '../common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  // 是否进行播放
+  playing: false,
+  // 是否进行全屏设置
+  fullScreen: false,
+  // 默认歌手的未排序的列表
+  playList: [],
+  // 播放排序后的列表
+  sequenceList: [],
+  // 播放模式
+  mode: playMode.sequence,
+  // 当前播放个的索引
+  currentIndex: -1
 }
 export default state
