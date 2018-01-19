e5b5fda3490c1c14d5ca433aa837166de3a63898
Goular <zhaojt_cs@126.com>
2018-01-19 11:27:55 +0800

    提交内容

--- 02项目实战/03.Vue-Music/src/common/js/Song.js
+++ 02项目实战/03.Vue-Music/src/common/js/Song.js
@@ -22,7 +22,7 @@ export function createSong(musicData) {
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
     // url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
     // 下面这个地址会经常变，只能到QQ音乐去抓这个网址的规律回来，再去拼接
-    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=1375586099&vkey=1BF59F9C073BE2B080C06CC95460E0DAC9BAEBDDCE3825C164942CBFC2150869EC0B09FAB181AF89D8D9F82E2EB0DD28AC35CAFE2CC6116E`
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=1375586099&vkey=FDAC1E4C410D5D5BCEE110870F69CF6984A8E99B2D4CBD17F5BCF07D72E913676C9D81851583E1AA84BC21CA2251DD852AA78761C5D09564`
   })
 }
 
