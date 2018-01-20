05157dc172efcfb4d58e875dab436171c711f7b2
Goular <zhaojt_cs@126.com>
2018-01-20 09:23:38 +0800

    提交内容

--- 02项目实战/03.Vue-Music/src/store/actions.js
+++ 02项目实战/03.Vue-Music/src/store/actions.js
@@ -1,10 +1,16 @@
 // 异步操作与异步响应
 import * as types from './mutation-types'
+import {playMode} from '../common/js/config'
+import {shuffle} from '../common/js/utils'
 
-export const selectPlay = function ({commit, state}, {list, index}) {
+export const selectPlay = function ({commit}, {list, index}) {
   commit(types.SET_SEQUENCE_LIST, list)
   commit(types.SET_PLAYLIST, list)
   commit(types.SET_CURRENT_INDEX, index)
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
+}
+
+export const randomPlay = function ({commit}, {list}) {
+  commit()
 }
