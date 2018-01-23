485effeae278d865fe3115105c11d2b1de356a82
Goular <zhaojt_cs@126.com>
2018-01-23 23:37:21 +0800

    内容提交

--- 02项目实战/03.Vue-Music/src/components/suggest/suggest.vue
+++ 02项目实战/03.Vue-Music/src/components/suggest/suggest.vue
@@ -6,7 +6,7 @@
           ref="suggest"
   >
     <ul class="suggest-list">
-      <li class="suggest-item" v-for="item in result">
+      <li @click="selectItem(item)" class="suggest-item" v-for="item in result">
         <div class="icon">
           <i :class="getIconCls(item)"></i>
         </div>
@@ -25,6 +25,8 @@
   import {createSong} from 'common/js/Song'
   import Scroll from '../../base/scroll/scroll'
   import Loading from 'base/loading/loading'
+  import Singer from 'common/js/singer'
+  import {mapMutations} from 'vuex'
 
   const TYPE_SINGER = 'singer'
   const perpage = 20
@@ -81,6 +83,18 @@
           }
         })
       },
+      selectItem(item) {
+        if (item.type === TYPE_SINGER) {
+          const singer = new Singer({
+            id: item.singermid,
+            name: item.singername
+          })
+          this.$router.push({
+            path: `/search/${singer.id}`
+          })
+          this.setSinger(singer)
+        }
+      },
       _checkMore(data) {
         const song = data.song
         if (!song.list.length || (song.curnum + song.curpage * 20) > song.totalnum) {
@@ -119,7 +133,10 @@
           }
         })
         return ret
-      }
+      },
+      ...mapMutations({
+        setSinger: 'SET_SINGER'
+      })
     },
     watch: {
       query() {
