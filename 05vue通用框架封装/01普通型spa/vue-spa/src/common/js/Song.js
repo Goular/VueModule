62b7e4c6e87942926a33e6395f15067aeb9ff789
Goular <zhaojt_cs@126.com>
2018-07-30 15:07:54 +0800

    添加前端的stylus

---
+++ 05vue通用框架封装/01普通型spa/vue-spa/src/common/js/Song.js
@@ -0,0 +1,92 @@
+import {getLyric} from '../../api/song'
+import {ERR_OK} from '../../api/config'
+import {Base64} from 'js-base64'
+
+export default class Song {
+  constructor({id, mid, singer, name, album, duration, image, url}) {
+    this.id = id
+    this.mid = mid
+    this.singer = singer
+    this.name = name
+    this.album = album
+    this.duration = duration
+    this.image = image
+    this.url = url
+  }
+
+  getLyric() {
+    if (this.lyric) {
+      return Promise.resolve(this.lyric)
+    }
+    return new Promise((resolve, reject) => {
+      getLyric(this.mid).then((res) => {
+        // if (res.retcode === ERR_OK) {
+        //   this.lyric = Base64.decode(res.lyric)
+        //   resolve(this.lyric)
+        // } else {
+        //   reject(new Error('no lyric'))
+        // }
+
+        if (res.status === 1) {
+          var ret = res.data
+          if (typeof ret === 'string') {
+            var reg = /^\w+\(({[^()]+})\)$/
+            var matches = ret.match(reg)
+            if (matches) {
+              ret = JSON.parse(matches[1])
+            }
+          }
+          if (ret.retcode === ERR_OK) {
+            this.lyric = Base64.decode(ret.lyric)
+            resolve(this.lyric)
+          } else {
+            reject(new Error('no lyric'))
+          }
+        }
+      })
+    })
+  }
+}
+
+const vkey = '6F318B07CB6F0106FC85D168D1DEE8C2BCDC311F20CDCF215AB6329462A267351EAA1935927430609919CF95DF884A0167D01A2F4406B4AF'
+
+export function createSong(musicData) {
+  return new Song({
+    id: musicData.songid,
+    mid: musicData.songmid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.songname,
+    album: musicData.albumname,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
+    // url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    // 下面这个地址会经常变，只能到QQ音乐去抓这个网址的规律回来，再去拼接
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=1375586099&vkey=${vkey}&uin=&fromtag=999`
+  })
+}
+
+export function createRecommendSong(musicData) {
+  return new Song({
+    id: musicData.id,
+    mid: musicData.mid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.name,
+    album: musicData.album.name,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.album.mid}.jpg?max_age=2592000`,
+    // url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    // 下面这个地址会经常变，只能到QQ音乐去抓这个网址的规律回来，再去拼接
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.mid}.m4a?guid=1375586099&vkey=${vkey}&uin=&fromtag=999`
+  })
+}
+
+function filterSinger(singer) {
+  let ret = []
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    ret.push(s.name)
+  })
+  return ret.join('/')
+}
---
+++ 05vue通用框架封装/01普通型spa/vue-spa/src/common/js/cache.js
@@ -0,0 +1,108 @@
+import storage from 'good-storage'
+
+// 搜索列表的最大值
+const SEARCH_KEY = '__search__'
+// 控制storage保存的最大数量
+const SEARCH_MAX_LENGTH = 15
+
+// 播放列表最大值
+const PLAY_KEY = '__play__'
+const PLAY_MAX_LENGTH = 200
+
+// 喜欢听的列表最大值
+const FAVORITE_KEY = '__favorite__'
+const FAVORITE_MAX_LENGTH = 200
+
+function insertArray(arr, val, compare, maxLen) {
+  const index = arr.findIndex(compare)
+  if (index === 0) {
+    return
+  }
+  if (index > 0) {
+    arr.splice(index, 1)
+  }
+  arr.unshift(val)
+  if (maxLen && arr.length > maxLen) {
+    arr.pop()
+  }
+}
+
+function deleteFromArray(arr, compare) {
+  const index = arr.findIndex(compare)
+  if (index > -1) {
+    arr.splice(index, 1)
+  }
+}
+
+export function saveSearch(query) {
+  let searches = storage.get(SEARCH_KEY, [])
+  insertArray(searches, query, (item) => {
+    return item === query
+  }, SEARCH_MAX_LENGTH)
+  storage.set(SEARCH_KEY, searches)
+  return searches
+}
+
+export function loadSearch() {
+  return storage.get(SEARCH_KEY, [])
+}
+
+export function deleteSearch(query) {
+  let searches = storage.get(SEARCH_KEY, [])
+  deleteFromArray(searches, (item) => {
+    return item === query
+  })
+  storage.set(SEARCH_KEY, searches)
+  return searches
+}
+
+// 清空资料的方法
+export function clearSearch() {
+  storage.remove(SEARCH_KEY)
+  return []
+}
+
+// 保存播放列表
+export function savePlay(song) {
+  let songs = storage.get(PLAY_KEY, [])
+  insertArray(songs, song, (item) => {
+    return item.id === song.id
+  }, PLAY_MAX_LENGTH)
+  storage.set(PLAY_KEY, songs)
+  return songs
+}
+
+// 加载播放列表
+export function loadPlay() {
+  return storage.get(PLAY_KEY, [])
+}
+
+// 清空播放列表
+export function clearPlay() {
+  return storage.get(PLAY_KEY, [])
+}
+
+// 保存喜爱歌曲
+export function saveFavorite(song) {
+  let songs = storage.get(FAVORITE_KEY, [])
+  insertArray(songs, song, (item) => {
+    return song.id === item.id
+  }, FAVORITE_MAX_LENGTH)
+  storage.set(FAVORITE_KEY, songs)
+  return songs
+}
+
+// 删除喜爱歌曲
+export function deleteFavorite(song) {
+  let songs = storage.get(FAVORITE_KEY, [])
+  deleteFromArray(songs, (item) => {
+    return song.id === item.id
+  })
+  storage.set(FAVORITE_KEY, songs)
+  return songs
+}
+
+// 加载喜爱歌曲列表
+export function loadFavorite() {
+  return storage.get(FAVORITE_KEY, [])
+}
