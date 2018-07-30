c79c17b2da1cd62f30f43f12a2350213cd16e8d7
Goular <zhaojt_cs@126.com>
2018-07-30 17:06:04 +0800

    添加内容

--- 05vue通用框架封装/01普通型spa/vue-spa/src/main.js
+++ 05vue通用框架封装/01普通型spa/vue-spa/src/main.js
@@ -1,8 +1,26 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+
+import 'common/stylus/index.styl'
+
+// 使用WechatFE-vConsole
+// import VConsole from 'vconsole'
+/* eslint-disable no-unused-vars */
+// var vConsole = new VConsole()
+// console.log('使用了vConsole')
 
 Vue.config.productionTip = false
+
+// fastclick配置
+fastclick.attach(document.body)
+// 添加vuelazyload的配置
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
